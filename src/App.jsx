fcca4e06fba7c1b58a5c347c98ad9ca7d6fc24d1
Dima Moskalenko <dmtrmsklnk@gmail.com>
2023-11-08 13:16:42 +0200

    save

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ const queryClient = new QueryClient();
 
 function App() {
   return <>
-  <QueryClientProvider  client={queryClient}>
+  <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />;
   </QueryClientProvider>
   </>
